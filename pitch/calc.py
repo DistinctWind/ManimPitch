934c6d5daa651d852b3e2e4950f24274c25cbb43
gorun <307532500@qq.com>
2024-11-12 23:50:34 +0800

    添加基本日志功能

--- pitch/calc.py
+++ pitch/calc.py
@@ -1,19 +1,28 @@
 import librosa
+import logging
 from pathlib import Path
 from .result import Result
 from .file import has_cache, cache_file_of, calculate_md5
 
+logger = logging.getLogger(__name__)
+
 
 def calculate_pitch(music: Path) -> Result:
+    logger.info(f"Calculate pitch for file {music}")
     cache_file = cache_file_of(music)
     md5 = calculate_md5(music)
+    logger.debug(f"Md5 for file {music} is '{md5}'")
 
     if has_cache(music):
         cache = Result.load(cache_file)
         if md5 == cache.md5:
+            logger.info(f"Use cache for file {music}")
             return cache
+        logger.info(
+            f"Cache file for file {music} is outdated"
+        )
 
-    # No cache, must calc
+    logger.info("Recalculate pitch for file {music}")
     y, sr = librosa.load(music, sr=None)
     C2_hz = float(librosa.note_to_hz("C2"))
     C7_hz = float(librosa.note_to_hz("C7"))
@@ -32,5 +41,6 @@ def calculate_pitch(music: Path) -> Result:
         md5=md5,
     )
 
+    logger.info(f"Store cache into file {cache_file}")
     result.store(cache_file)
     return result
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from pitch.file import calculate_md5
 from pitch.calc import calculate_pitch
 from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s][%(levelname)s] %(message)s",
+)
 
 print(calculate_md5(Path("vocal.wav")))
 result = calculate_pitch(Path("vocal.wav"))
